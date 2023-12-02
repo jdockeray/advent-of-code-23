42bcf543ea09e9949c1f585255e4adbb25d86b50
James Dockeray <2040040+jdockeray@users.noreply.github.com>
2023-12-02 08:32:16 +0000

    feat: day part 2

--- lib/day1/utils.py
+++ lib/day1/utils.py
@@ -1,15 +1,84 @@
 from typing import List
 
+words: dict[str, str] = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+
+class NumsIterator:
+
+    def __init__(self, data, reverse=False):
+        self.data = data
+        self.index = reverse and len(data) - 1 or 0
+        self.reverse = reverse
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        index = self.index
+        reverse = self.reverse
+        data = self.data
+
+        if (not reverse and index >= len(data)) or (reverse and index < 0):
+            raise StopIteration
+
+        value = data[index]
+
+        if value.isdigit():
+            index = reverse and index - 1 or index + 1
+            return value
+
+        for word, digit in words.items():
+            left = index
+            right = index + len(word)
+
+            if reverse:
+                left = index - (len(word) - 1)
+                right = index + 1
+
+            if data[left:right] == word:
+                if reverse:
+                    index -= len(word)
+                else:
+                    index += len(word)
+                return digit
+        if reverse:
+            self.index = index - 1
+        else:
+            self.index = index + 1
+
+        return self.__next__()
+
 
 def parse_input(path: str) -> List[str]:
     with open(path, 'r') as f:
         return f.read().splitlines()
 
 
+def first(input: List[str]) -> str:
+    nums = NumsIterator(input)
+    next_value = next(nums)
+    return next_value
+
+
+def last(input: List[str]) -> int:
+    nums = NumsIterator(input, reverse=True)
+    next_value = next(nums)
+    return next_value
+
+
 def convert(strings: List[str]) -> int:
-    result = 0
+    result: int = 0
 
     for string in strings:
-        filtered = list(filter(lambda char: char.isdigit(), string))
-        result += int(filtered[0] + filtered[-1])
+        result += int(first(string) + last(string))
     return result
--- tests/day1/test_run.py
+++ tests/day1/test_run.py
@@ -1,13 +1,15 @@
-from typing import List
-from lib.day1.utils import convert, parse_input
+import pytest
+from lib.day1.utils import convert, parse_input, first, last
 from unittest.mock import mock_open, patch
+from typing import List
 
-sample: List[str] = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
 
-
-def test_convert() -> None:
-    result: int = convert(sample)
-    assert result == 142, "Conversion failed"
+@pytest.mark.parametrize(
+    "input,expected", [[["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"],
+                        142], [["two1nine"], 29], [["eightwothree"], 83]])
+def test_convert_simple(input, expected) -> None:
+    result: int = convert(input)
+    assert result == expected, "Conversion failed"
 
 
 def test_read_file() -> None:
@@ -15,3 +17,20 @@ def test_read_file() -> None:
         result = parse_input('file.txt')
         assert result == ["1", "2", "3"], "Reading file failed"
         mock_file.assert_called_once_with('file.txt', 'r')
+
+
+@pytest.mark.parametrize("str,expected",
+                         [["1abc2", "1"], ["two1nine", "2"],
+                          ["eightwothree", "8"], ["treb7uchet", "7"]])
+def test_first(str, expected) -> None:
+    result: int = first(str)
+    assert result == expected, "First element is incorrect"
+
+
+@pytest.mark.parametrize(
+    "str,expected",
+    [["1abc2", "2"], ["two1nine", "9"], ["eightwothree", "3"],
+     ["treb7uchet", "7"], ["6lpxrrhvdslxgpjblcmgsgbdpdkfmzkr", "6"]])
+def test_last(str, expected) -> None:
+    result: int = last(str)
+    assert result == expected, "Last element is incorrect"
