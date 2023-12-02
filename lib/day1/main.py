178dd713268568d6041d60334a4f80092f6bcff0
James Dockeray <2040040+jdockeray@users.noreply.github.com>
2023-12-02 10:11:15 +0000

    chore: move data to dir

--- lib/day1/main.py
+++ lib/day1/main.py
@@ -1,9 +1,8 @@
 from lib.day1.utils import convert, parse_input
-import os
 
 
 def main():
-    print(convert(parse_input('lib/day1/input.txt')))
+    print(convert(parse_input('data/day1/input.txt')))
 
 
 if __name__ == "__main__":
